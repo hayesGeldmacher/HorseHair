0e0c2c55df108c5925ce41a6eb85d584e4860500
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2026-06-18 00:03:58 -0600

    Added Eyelids to fighting game scene - HG

---
+++ Assets/Project/Scripts/FighterMiniGame/EyelidsFG.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class EyelidsFG : MonoBehaviour
+{
+
+    [Header("Animation Settings")]
+    [SerializeField] private float blinkAnimationSpeed = 0.5f;
+    [SerializeField] private Animator blinkAnimator;
+    private bool hasBlinked = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!hasBlinked)
+        {
+            hasBlinked = true;
+            StartCoroutine(TriggerBlinkAnimation());
+        }
+    }
+
+    //function for unique first blinking animation
+    private IEnumerator TriggerBlinkAnimation()
+    {
+ 
+        blinkAnimator.SetFloat("AnimationSpeed", blinkAnimationSpeed);
+        yield return new WaitForSeconds(0.4f);
+        blinkAnimator.SetTrigger("EyesStart");
+
+    }
+}
---
+++ Assets/Project/Scripts/transitions/TransitionManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TransitionManager : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource transitionSource; //the primary audio source for the transition - HG
+
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
