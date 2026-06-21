e2a866ef74c05d766e99819cc7831aff22c97abc
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2026-06-20 18:13:04 -0600

    Added lighting, audio to dream scene -HG

---
+++ Assets/Project/Scripts/Dreams/DreamLightingTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DreamLightingTrigger : MonoBehaviour
+{
+
+    [Header("Directional Light")]
+    [SerializeField] private bool triggerDirectional = true;
+    [SerializeField] private float directionalTarget;
+
+    [Header("Ambient Light")]
+    [SerializeField] private bool triggerAmbience = true;
+    [SerializeField] private float ambientTarget;
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered)
+        {
+            triggered = true;
+
+            if (triggerDirectional) { DreamLightingManager.instance.SetDirectionalTarget(directionalTarget); }
+            if (triggerAmbience) { DreamLightingManager.instance.SetAmbientTarget(ambientTarget); }
+        }
+    }
+
+}
