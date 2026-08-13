62e32beaa3d4b1d7a961525bdb7badf49d9826d2
ShenesRaint <88374900@qq.com>
2026-08-13 12:05:01 -0600

    Ryan Bake Light Test
    Add Light Test
    Its in the Scene folder

---
+++ Assets/Project/Scenes/SCN_TEST/SCRIPT/Light_Flickering.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Light_Flickering : MonoBehaviour
+{
+
+    private Light light;
+
+    public float minIntensity = .5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.1f;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
+    }
+
+    private void Flicker()
+    {
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;
+    }
+
+
+}
